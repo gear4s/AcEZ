14bddfb90f8939d53e994cb5ed297f10cf66d1bf
Aaron Narwhaal <aaron@graphox.us>
2017-03-06 15:55:45 +0200

    Create Definitions.lua
    Incomplete

---
+++ Definitions.lua
@@ -0,0 +1,83 @@
+--[[
+
+  AcEZ - by gear4
+  Wrapper for Ace3
+  
+  Base Ace3 widgets
+  
+  Copyright (c) 2017, Aaron Marais
+
+]]
+
+local name, addon = ...
+local AceGUI = LibStub("AceGUI-3.0")
+local AH = addon.AceHelper
+
+local frameTable = AH.createTable{
+  title = "SetTitle",
+  layout = "SetLayout",
+  statustext = "SetStatusText",
+  child = function(self, child)
+    child.par = self
+    self.obj:AddChild(child.obj)
+    return self
+  end
+}
+
+local buttonTable = AH.createTable{
+  text = "SetText",
+  width = "SetWidth",
+  disabled = "SetDisabled",
+}
+
+local checkboxTable = AH.createTable{
+  val = "SetValue",
+  type = "SetType",
+  img = "SetImage",
+  label = "SetLabel",
+  desc = "SetDescription",
+  tristate = "SetTriState",
+  disabled = "SetDisabled",
+  toggle = "ToggleChecked",
+}
+
+local dropdownTable = AH.createTable{
+  val = "SetValue",
+  list = "SetList",
+  text = "SetText",
+  item = "AddItem",
+  label = "SetLabel",
+  multi = "SetMultiselect",
+  itemval = "SetItemValue",
+  itemdisabled = "SetItemDisabled",
+}
+
+local colorpickerTable = AH.createTable{
+  color = "SetColor",
+  label = "SetLabel",
+  alpha = "SetHasAlpha",
+  disabled = "SetDisabled",
+}
+
+local editboxTable = AH.createTable{
+  text = "SetText",
+  label = "SetLabel",
+  disabled = "SetDisabled",
+  disablebutton = "DisableButton",
+  maxchar = "SetMaxLetters",
+  focus = "SetFocus",
+  hltext = "HighlightText"
+}
+
+local headingTable = AH.createTable{
+  text = "SetText",
+}
+
+AH.createType("Frame", frameTable, function(t, txt) t:SetTitle(txt) end)
+AH.createType("Button", buttonTable, function(t, txt) t:SetText(txt) end)
+AH.createType("CheckBox", checkboxTable, function(t, txt) t:SetLabel(txt) end)
+AH.createType("Dropdown", dropdownTable, function(t, txt) t:SetLabel(txt) end)
+AH.createType("ColorPicker", colorpickerTable, function(t, txt) t:SetLabel(txt) end)
+AH.createType("EditBox", editboxTable, function(t, txt) t:SetLabel(txt) end)
+AH.createType("Heading", editboxTable, function(t, txt) t:SetText(txt) end)
+
